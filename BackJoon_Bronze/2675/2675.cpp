3e42f6f55c967b999edd952f22d5cc3cf00d38c2
kyungwon <kyungwon0928@daum.net>
2023-11-12 02:02:08 +0900

    Bronze2_2675. 문자열 반복

---
+++ BackJoon_Bronze/2675/2675.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+    int T, R;
+    string S;
+
+    cin >> T;    
+
+    for(int i = 0; i<T;i++) {
+        cin >> R >> S;
+        for(int k = 0; k<S.length(); k++) {
+            for(int j = 0; j<R; j++){
+                cout << S[k];
+            }
+        }
+        cout << endl;
+    }
+}
