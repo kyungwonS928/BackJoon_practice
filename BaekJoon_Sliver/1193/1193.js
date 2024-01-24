6b87d80ca1f0a9358a34b5528d04a0174565903a
kyungwon <kyungwon0928@daum.net>
2024-01-25 03:06:28 +0900

    Silver5_1193. 분수찾기

--- BaekJoon_Sliver/1193/1193.js
+++ BaekJoon_Sliver/1193/1193.js
@@ -8,13 +8,26 @@ let input = [];
 readline.on('line', function (line) {
     line.split(' ').map(el => input.push(parseInt(el)));
 }).on('close', function () { 
-    let count = 1;
-    let rooms = 1;
-    for(let i = 1; count < input[0]; i++) {
-        count += i*6;
-        rooms++;
+    let count = input[0];
+    let line = 1;
+    let total = 0;
+    let result = '';
+
+
+    for(line; total + line<count; line++) {
+        total += line;
     }
 
-    console.log(rooms);
+
+    if(line % 2 == 0) {
+
+        result = String(1+ (count - total -1)) + '/' + String(line- (count-total-1))
+    } else {
+        
+    result = String(line- (count-total-1)) + '/' + String(1+ (count - total -1))
+    }
+
+
+    console.log(result);
 });
 
