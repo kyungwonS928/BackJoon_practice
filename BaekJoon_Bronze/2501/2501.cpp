dc9279e6793251686a0e81edd6ecad8f82c42c60
kyungwon <kyungwon0928@daum.net>
2024-01-25 22:19:30 +0900

    Bronze3_2501. 약수 구하기

---
+++ BaekJoon_Bronze/2501/2501.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    int N, K;
+    int flag = 0;
+    //초기화를 하지 않으면 해당 변수는 쓰레기값을 가지게 되기 때문에 예상치 못한 결과가 나올 수도 있음 -> 어떤 값이 들어있는지 확신할 수 없음
+
+    cin >> N >> K;
+
+    for(int i = 1; i<=N; i++) {
+        if(N % i == 0) {
+            flag++;
+            if(flag == K) {
+                cout << i;
+                return 0;
+            }
+        }
+    }
+
+    cout << "0";
+
+    return 0;
+
+}
