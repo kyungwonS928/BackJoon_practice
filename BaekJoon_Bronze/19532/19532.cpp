9d3604627b890fdbda693d10d4149f6da7b8ea73
kyungwon <kyungwon0928@daum.net>
2024-01-30 20:42:32 +0900

    Bronze2_19532. 수학은 비대면 강의입니다 ver2

--- BaekJoon_Bronze/19532/19532.cpp
+++ BaekJoon_Bronze/19532/19532.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<string>
 using namespace std;
 
 
@@ -10,11 +9,16 @@ int main() {
 
     int x, y;
 
-    for(x = -999; x<= 999; x++) {
-        for(y = -999; y <= 999; y++) {
-            if((a*x) + (b*y) == c && (d*x) + (e*y) ==f) {
-                cout << x <<" "<<y;
-            }
-        }
-    }
+    // for(x = -999; x<= 999; x++) {
+    //     for(y = -999; y <= 999; y++) {
+    //         if((a*x) + (b*y) == c && (d*x) + (e*y) ==f) {
+    //             cout << x <<" "<<y;
+    //         }
+    //     }
+    // }
+
+    x = (c * e) - (b*f) / (a*e) - (b*d);
+    y = (c * d) - (a*f) / (b*d) - (a*e);
+
+    cout << x << " " << y;
 }
