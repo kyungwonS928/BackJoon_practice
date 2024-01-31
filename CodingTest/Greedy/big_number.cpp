5849a672131f631b6d83b6cd6f37862eb0b5ad2b
kyungwon <kyungwon0928@daum.net>
2024-01-31 16:24:00 +0900

    큰 수의 법칙 - M이 매우 큰 수 일때
    - 반복되는 수열에 대해서 파악
      - 가장 큰 수를 K번 + 두번째로 큰 수를 1번
      - M / (K+1) : 수열 반복회수

--- CodingTest/Greedy/big_number.cpp
+++ CodingTest/Greedy/big_number.cpp
@@ -3,6 +3,29 @@
 using namespace std;
 
 
+// int main() {
+//     int num[1000];
+//     int N, M, K;
+//     cin >> N >> M >> K;
+//     int result = 0;
+
+//     for(int i = 0; i<N; i++){
+//         cin >> num[i];
+//     }
+//     sort(num, num+N);
+
+//     for(int i = 0; i < M; i++) {
+//         for(int j = 0; j < K; j++) {
+//             result += num[N-1];
+//             i++;
+//         }
+//         result += num[N-2];
+//     }
+
+//     cout << result;
+//     return 0;
+// }
+
 int main() {
     int num[1000];
     int N, M, K;
@@ -13,14 +36,12 @@ int main() {
         cin >> num[i];
     }
     sort(num, num+N);
+    
+    int count = (M/(K+1)) * K + M%(K+1);
 
-    for(int i = 0; i < M; i++) {
-        for(int j = 0; j < K; j++) {
-            result += num[N-1];
-            i++;
-        }
-        result += num[N-2];
-    }
+
+    result += count * num[N-1];
+    result += (M-count) * num[N-2];
 
     cout << result;
     return 0;
