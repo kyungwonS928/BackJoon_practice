347469df9d523c8428a13e872ac6ae48a6c015af
kyungwon <kyungwon0928@daum.net>
2024-02-08 03:37:09 +0900

    Silver5_11651. 좌표 정렬하기 2

---
+++ BaekJoon_Sliver/11651/11651.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool compare(pair<int, int> i, pair<int, int> j) {
+    if(i.second == j.second) {
+        return i.first < j.first;
+    }
+    
+    return i.second < j.second;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<pair<int, int>> num;
+    int N;
+    cin >> N;
+
+    for(int i = 0; i<N; i++) {
+        int x, y;
+        cin >> x >>y;
+        num.push_back({x, y});
+    }
+    
+    sort(num.begin(), num.begin() + N, compare);
+
+    for(int i = 0; i<N; i++) {
+        cout << num[i].first << " " << num[i].second << '\n';
+    } 
+    return 0;
+
+}
