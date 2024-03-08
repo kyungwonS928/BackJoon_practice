070a5d03366979f19266495a77499dfddebe3ba2
kyungwon <kyungwon0928@daum.net>
2024-03-08 23:21:02 +0900

    Bronze4_3046. R2

---
+++ BaekJoon_Bronze/3046/3046.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int R1, S;
+    cin >> R1 >> S;
+
+
+
+    cout << (2 * S) - R1;
+    return 0;
+
+}
