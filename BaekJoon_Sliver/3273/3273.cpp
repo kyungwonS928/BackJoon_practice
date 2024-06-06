cddcb6ad2a052c5618e8ca5a1a0e3cfcded529a7
kyungwon <kyungwon0928@daum.net>
2024-06-06 17:18:01 +0900

    Silver3_3273. 두 수의 합

---
+++ BaekJoon_Sliver/3273/3273.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int num[100001];
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, tmp, x;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num[i];
+	}
+	cin >> x;
+	sort(num, num + n);
+	int sum = 0;
+	int left = 0, right = n - 1;
+	while (left < right)
+	{
+		if (num[left] + num[right] == x)
+		{
+			sum += 1;
+			left += 1;
+			right -= 1;
+		}
+		else if (num[left] + num[right] > x)
+		{
+			right -= 1;
+		}
+		else
+		{
+			left += 1;
+		}
+	}
+	cout << sum;
+
+	return 0;
+}
