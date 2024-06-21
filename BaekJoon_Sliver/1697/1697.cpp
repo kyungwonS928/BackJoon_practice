4569328ce5a5189dedd0e422d3cc3558303171de
kyungwon <kyungwon0928@daum.net>
2024-06-21 19:20:56 +0900

    1697 - 문제 풀이 조건 고려하기

--- BaekJoon_Sliver/1697/1697.cpp
+++ BaekJoon_Sliver/1697/1697.cpp
@@ -3,38 +3,51 @@
 using namespace std;
 int n, k;
 bool visit[100001];
-void bfs(int a){
+
+// 수빈이가 0에서 100000에서만 움직인다는 보장 없음
+
+void bfs(int a)
+{
     queue<pair<int, int>> q;
     q.push(make_pair(a, 0));
-    while(!q.empty()){
+    while (!q.empty())
+    {
         int x = q.front().first;
         int cnt = q.front().second;
         q.pop();
-        if(x==k){
+        if (x == k)
+        {
             cout << cnt;
             break;
         }
-        if(x+1 >=0 && x+1<100001){
-            if(!visit[x+1] ){
+        if (x + 1 >= 0 && x + 1 < 100001)
+        {
+            if (!visit[x + 1])
+            {
                 visit[x + 1] = true;
                 q.push(make_pair(x + 1, cnt + 1));
             }
         }
-        if(x-1 >=0 && x-1<100001){
-            if(!visit[x-1]){
+        if (x - 1 >= 0 && x - 1 < 100001)
+        {
+            if (!visit[x - 1])
+            {
                 visit[x - 1] = true;
                 q.push(make_pair(x - 1, cnt + 1));
             }
         }
-        if(2*x >=0 && 2*x<100001){
-            if(!visit[2*x]){
-                visit[2*x] = true;
+        if (2 * x >= 0 && 2 * x < 100001)
+        {
+            if (!visit[2 * x])
+            {
+                visit[2 * x] = true;
                 q.push(make_pair(2 * x, cnt + 1));
             }
         }
     }
 }
-int main(){
+int main()
+{
     cin >> n >> k;
     visit[n] = true;
     bfs(n);
