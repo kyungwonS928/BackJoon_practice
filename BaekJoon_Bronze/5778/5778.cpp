883f1b22c25c80e38bb5bbd1a9b6e8b40e8633b1
kyungwon <kyungwon0928@daum.net>
2024-06-28 17:33:01 +0900

    Silver5_2535. 아시아 정보올림피아드

--- BaekJoon_Bronze/5778/5778.cpp
+++ BaekJoon_Bronze/5778/5778.cpp
@@ -1,33 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool compare(tuple<int, int, int> i, tuple<int, int, int> j)
+{
+	int first = get<2>(i);
+	int second = get<2>(j);
+
+	if (first > second)
+		return true;
+	return false;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	while (true)
+	int N;
+
+	cin >> N;
+
+	vector<tuple<int, int, int>> score;
+
+	for (int i = 0; i < N; i++)
 	{
-		int N, M;
-		cin >> N >> M;
-
-		if (N == 0 && M == 0)
-			break;
-
-		int copied = 0;
-		int num[10001] = {0};
-
-		while (M--)
-		{
-			int ticket;
-			cin >> ticket;
-			if (num[ticket] == 1)
-			{
-				copied++;
-			}
-			num[ticket]++;
-		}
-
-		cout << copied << '\n';
+		int a, b, c;
+		cin >> a >> b >> c;
+		score.push_back({a, b, c});
 	}
+
+	sort(score.begin(), score.end(), compare);
+
+	int first = get<0>(score[0]), second = get<0>(score[1]);
+	int idx = 2;
+
+	if (first == second)
+	{
+		for (int i = 2; i < N; i++)
+		{
+			int third = get<0>(score[i]);
+			if (third != first)
+			{
+				idx = i;
+				break;
+			}
+		}
+	}
+
+	cout << get<0>(score[0]) << " " << get<1>(score[0]) << '\n';
+	cout << get<0>(score[1]) << " " << get<1>(score[1]) << '\n';
+	cout << get<0>(score[idx]) << " " << get<1>(score[idx]) << '\n';
 }
