e5629f9819cabdb4f97a60d4cd00e8367a5db923
kyungwon <kyungwon0928@daum.net>
2024-07-03 23:11:03 +0900

    (최적화) Bronze3_11134. 쿠키애호가

--- BaekJoon_Bronze/11134/11134.cpp
+++ BaekJoon_Bronze/11134/11134.cpp
@@ -2,6 +2,38 @@
 #include <stack>
 using namespace std;
 
+// int main()
+// {
+
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(NULL);
+
+// 	int T;
+// 	cin >> T;
+// 	while (T--)
+// 	{
+// 		int day = 0;
+// 		int N, C;
+// 		cin >> N >> C;
+
+// 		while (N)
+// 		{
+// 			if (N < C)
+// 			{
+// 				N = 0;
+// 				day++;
+// 			}
+// 			else
+// 			{
+// 				N -= C;
+// 				day++;
+// 			}
+// 		}
+
+// 		cout << day << '\n';
+// 	}
+// }
+
 int main()
 {
 
@@ -16,19 +48,9 @@ int main()
 		int N, C;
 		cin >> N >> C;
 
-		while (N)
-		{
-			if (N < C)
-			{
-				N = 0;
-				day++;
-			}
-			else
-			{
-				N -= C;
-				day++;
-			}
-		}
+		day = N / C;
+		if (N % C != 0)
+			day++;
 
 		cout << day << '\n';
 	}
