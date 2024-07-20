c9c4e967affa9add6f1573df694d7cc3e7103471
kyungwon <kyungwon0928@daum.net>
2024-07-20 21:23:27 +0900

    Bronze4_2440. 별 찍기-3

---
+++ BaekJoon_Bronze/2440/2440.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = N; i > 0; i--)
+	{
+		for (int j = i; j > 0; j--)
+		{
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
